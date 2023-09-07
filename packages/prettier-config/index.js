4d1b74bc2f3fab7b72478114f2a3c31c82aa5b34
Vahor <nathan.d0601@gmail.com>
2023-09-07 11:37:34 +0200

    parametrable tailwindConfig path

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,18 +1,12 @@
-/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
-/** @typedef  {import("prettier").Config} PrettierConfig*/
-/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
-
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
   plugins: [
-    require.resolve("@ianvs/prettier-plugin-sort-imports"),
-    require.resolve("prettier-plugin-tailwindcss"),
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
   ],
   semi: true,
   trailingComma: "all",
   singleQuote: true,
   printWidth: 100,
-  tailwindConfig: "./tailwind.config.cjs",
   arrowParens: "avoid",
   tabWidth: 2,
 };
