678a82510e981476e4c86e12c5ec8988789b0fca
Vahor <nathan.d0601@gmail.com>
2023-09-17 21:17:05 +0200

    smaller container

--- packages/design/src/tailwind/tailwind.config.ts
+++ packages/design/src/tailwind/tailwind.config.ts
@@ -24,10 +24,20 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "3rem",
+        lg: "6rem",
+        xl: "7.5rem",
+        "2xl": "9rem"
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        '2xl': "1400px"
+      }
     },
     extend: {
       colors: {
