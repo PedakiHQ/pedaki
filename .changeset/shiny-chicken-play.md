951a2dc2b8eef8a144584668ffcf52de3eda580a
Vahor <nathan.d0601@gmail.com>
2023-09-17 17:53:38 +0200

    add gray gradient variant separator

--- .changeset/shiny-chicken-play.md
+++ .changeset/shiny-chicken-play.md
@@ -3,3 +3,6 @@
 ---
 
 Simpler color system
+
+Add Separator gradient. 
+You can now use `gradient="gray"` to have a gray gradient separator.
--- packages/design/src/ui/separator.tsx
+++ packages/design/src/ui/separator.tsx
@@ -1,22 +1,41 @@
 'use client';
 
 import * as SeparatorPrimitive from '@radix-ui/react-separator';
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
 import * as React from 'react';
 import { cn } from '../utils';
 
+const separatorVariants = cva('bg-border shrink-0', {
+  variants: {
+    orientation: {
+      horizontal: 'h-[1px] w-full',
+      vertical: 'h-full w-[1px]',
+    },
+    gradient: {
+      none: '',
+      gray: 'bg-gradient-to-r from-gray-50 via-gray-200 to-gray-50',
+    },
+  },
+  defaultVariants: {
+    orientation: 'horizontal',
+    gradient: 'none',
+  },
+});
+
+export interface SeparatorProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof separatorVariants> {}
+
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(({ className, orientation = 'horizontal', decorative = true, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root> & SeparatorProps
+>(({ className, orientation = 'horizontal', gradient, decorative = true, ...props }, ref) => (
   <SeparatorPrimitive.Root
     ref={ref}
     decorative={decorative}
     orientation={orientation}
-    className={cn(
-      'bg-border shrink-0',
-      orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
-      className,
-    )}
+    className={cn(separatorVariants({ orientation, gradient }), className)}
     {...props}
   />
 ));
