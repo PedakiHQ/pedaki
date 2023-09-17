318442072197a09064c78915758be003caeb25d0
Vahor <nathan.d0601@gmail.com>
2023-09-17 21:16:57 +0200

    fix skeleton size

--- packages/design/src/ui/skeleton.tsx
+++ packages/design/src/ui/skeleton.tsx
@@ -1,7 +1,8 @@
 import { cn } from '../utils';
+import React from "react";
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('bg-tertiary animate-pulse rounded-md', className)} {...props} />;
+  return <div className={cn('bg-tertiary animate-pulse rounded-md h-full w-full', className)} {...props} />;
 }
 
 export { Skeleton };
