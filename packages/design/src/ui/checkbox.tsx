73c0440203912741ee2447281dc9d9ed3d0e0392
Vahor <nathan.d0601@gmail.com>
2023-10-07 17:44:44 +0200

    format

--- packages/design/src/ui/checkbox.tsx
+++ packages/design/src/ui/checkbox.tsx
@@ -1,10 +1,9 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-
-import { cn } from "~/utils"
-import IconCheck from "~/ui/icons/IconCheck.tsx";
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import IconCheck from '~/ui/icons/IconCheck.tsx';
+import { cn } from '~/utils';
+import * as React from 'react';
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -13,18 +12,16 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer inline-flex h-4 w-4 shrink-0 rounded-sm border ring-offset-bg-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-secondary data-[state=checked]:text-primary",
-      className
+      'ring-offset-bg-primary focus-visible:ring-orange data-[state=checked]:bg-secondary data-[state=checked]:text-primary peer inline-flex h-4 w-4 shrink-0 rounded-sm border focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+      className,
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
+    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
       <IconCheck className="h-3.5 w-3.5" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox }
+export { Checkbox };
--- packages/design/src/ui/icons/IconCircle.tsx
+++ packages/design/src/ui/icons/IconCircle.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import type {IconProps} from './type.ts';
+import type { IconProps } from './type.ts';
 
-// Lucide icon: https://lucide.dev/icons/circlehttps://lucide.dev/icons/arrow-right
+// Lucide icon: https://lucide.dev/icons/circle
 const IconCircle = (props: IconProps) => (
-    <svg role="img" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-         strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <circle cx="12" cy="12" r="10"/>
-    </svg>
+  <svg
+    role="img"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <circle cx="12" cy="12" r="10" />
+  </svg>
 );
 
 export default IconCircle;
