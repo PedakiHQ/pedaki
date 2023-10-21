7495f094b9204fd13631a07bb1b92220f540792c
Vahor <nathan.d0601@gmail.com>
2023-10-21 13:51:26 +0200

    add IconPedaki

---
+++ .changeset/seven-gifts-mix.md
@@ -0,0 +1,5 @@
+---
+"@pedaki/design": patch
+---
+
+add IconPedaki icon
--- packages/design/src/ui/icons/index.ts
+++ packages/design/src/ui/icons/index.ts
@@ -1,7 +1,5 @@
 export type { IconType, IconProps } from './type.ts';
 
-export { default as IconGoogle } from './IconGoogle.tsx';
-export { default as IconMicrosoft } from './IconMicrosoft.tsx';
 export { default as IconSpinner } from './IconSpinner.tsx';
 export { default as IconArrowRight } from './IconArrowRight.tsx';
 export { default as IconChevronRight } from './IconChevronRight.tsx';
@@ -14,3 +12,8 @@ export { default as IconCircle } from './IconCircle.tsx';
 // Flags
 export { default as IconFlagGB } from './IconFlagGB.tsx';
 export { default as IconFlagFR } from './IconFlagFR.tsx';
+
+// Logo
+export { default as IconPedaki } from './IconPedaki.tsx';
+export { default as IconGoogle } from './IconGoogle.tsx';
+export { default as IconMicrosoft } from './IconMicrosoft.tsx';
