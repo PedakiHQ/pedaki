5c2ddbea1bb07936b1ccd0574c42cb125eb3c9c0
Vahor <nathan.d0601@gmail.com>
2023-10-21 14:51:31 +0200

    remove large padding

--- packages/design/src/tailwind/tailwind.config.ts
+++ packages/design/src/tailwind/tailwind.config.ts
@@ -36,8 +36,7 @@ export default {
         DEFAULT: '1rem',
         sm: '2rem',
         lg: '4rem',
-        xl: '7.5rem',
-        '2xl': '9rem',
+        xl: '7.5rem'
       },
       screens: {
         sm: '640px',
