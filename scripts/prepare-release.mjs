05aaba78fe5dfb9a8b1233cd626aa3a546443b8c
Nathan <me@vahor.fr>
2023-10-30 18:55:07 +0100

    refactor: remove changesets (#86)

---
+++ scripts/prepare-release.mjs
@@ -0,0 +1,140 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { getPackages } from "@manypkg/get-packages";
+import {$} from 'execa';
+import ora from "ora";
+import open from "open";
+import fs from "fs";
+import path from "path";
+
+const githubBaseUrl = 'https://github.com/pedakihq/pedaki';
+const packages = await getPackages(process.cwd());
+const currentVersion = packages.rootPackage.packageJson.version;
+
+const questions = [
+    {
+        type: 'list',
+        name: 'versionType',
+        message: 'What type of version do you want to release?',
+        choices: [
+            {name: "Don't change", value: 'skip'},
+            {name: 'Major', value: 'major'},
+            {name: 'Minor', value: 'minor'},
+            {name: 'Patch', value: 'patch'},
+            {name: "Manual", value: "manual"},
+        ],
+    },
+    {
+        type: 'input',
+        name: 'version',
+        message: 'What version do you want to release?',
+        when: (answers) => answers.versionType === 'manual',
+    },
+    {
+        type: 'confirm',
+        name: 'preRelease',
+        message: 'Is this a pre-release?',
+        default: true
+    }
+];
+
+const nextVersion = (versionType) => {
+    const versionParts = currentVersion.split('.');
+    const major = parseInt(versionParts[0]) || 0;
+    const minor = parseInt(versionParts[1]) || 0;
+    const patch = parseInt(versionParts[2]) || 0;
+
+    switch (versionType) {
+        case "major":
+            return `${major + 1}.0.0`;
+        case "minor":
+            return `${major}.${minor + 1}.0`;
+        case "patch":
+            return `${major}.${minor}.${patch + 1}`;
+        case "skip":
+            return `${major}.${minor}.${patch}`;
+        default:
+            throw new Error('Invalid version type!');
+    }
+}
+
+const preReleaseSuffix = (useOld, preRelease) => {
+    if (preRelease) {
+        const versionParts = currentVersion.split('-', 2);
+        if (!useOld || versionParts.length === 1) {
+            return '-beta.0';
+        }
+        const preReleaseParts = versionParts[1].split('.');
+        const preReleaseNumber =  (parseInt(preReleaseParts[1]) + 1) || 0;
+        return `-beta.${preReleaseNumber}`;
+    }
+    return '';
+}
+
+const updateDependencies = (dependencies, newVersion) => {
+    if (!dependencies) return;
+    for (const packageName of Object.keys(dependencies)) {
+        if (packageName.startsWith('@pedaki')) {
+            dependencies[packageName] = newVersion;
+        }
+    }
+    return dependencies;
+}
+
+const updatePackageJson = async (newVersion) => {
+    const spinner = ora('Updating package.json files...');
+    spinner.start();
+
+    const packageJsonFiles = [
+        ...packages.packages.map((pkg) => path.join(pkg.dir, 'package.json')),
+        path.join(packages.rootPackage.dir, 'package.json')
+    ]
+
+    for (const packageJsonFile of packageJsonFiles) {
+        console.log(`Updating ${chalk.cyan(path.basename(path.dirname(packageJsonFile)))}...`);
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonFile));
+        packageJson.version = newVersion;
+        packageJson.dependencies = updateDependencies(packageJson.dependencies, newVersion);
+        packageJson.devDependencies = updateDependencies(packageJson.devDependencies, newVersion);
+        fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null, 2));
+    }
+
+    spinner.succeed();
+}
+
+const updateLockFiles = async () => {
+    const spinner = ora('Updating lock files...');
+    spinner.start();
+    await $`pnpm i`;
+    spinner.succeed();
+}
+
+const openNewTagPage = async (newVersion, preRelease) => {
+    const spinner = ora('Opening new tag page...');
+    spinner.start();
+    await open(`${githubBaseUrl}/releases/new?tag=v${newVersion}&title=v${newVersion}&prerelease=${preRelease}`);
+    spinner.succeed();
+}
+
+const checkCurrentBranch = async () => {
+    const {stdout} = await $`git branch --show-current`;
+    const currentBranch = stdout.trim();
+    if (currentBranch !== 'main') {
+        // console.error(`You are on ${chalk.cyan(currentBranch)} branch. Please switch to ${chalk.cyan('main')} branch.`);
+        // process.exit(1);
+    }
+}
+
+await checkCurrentBranch();
+
+inquirer.prompt(questions).then(async (answers) => {
+    const {versionType} = answers;
+    console.log(`You have selected to release a ${chalk.cyan(versionType)} version.`);
+    let newVersion = answers.version || nextVersion(versionType);
+    newVersion += preReleaseSuffix(answers.versionType === 'skip', answers.preRelease);
+    console.log(`The new version will be ${chalk.cyan(newVersion)}.`);
+    await updatePackageJson(newVersion);
+    await updateLockFiles();
+    await openNewTagPage(newVersion, answers.preRelease);
+});
+
