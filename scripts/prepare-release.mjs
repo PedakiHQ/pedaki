ea8c059f5e2bfb1186dc1578c12aa775c8be99d4
Vahor <nathan.d0601@gmail.com>
2023-11-03 13:43:21 +0100

    fix: revertChange -> pull

--- scripts/prepare-release.mjs
+++ scripts/prepare-release.mjs
@@ -155,7 +155,7 @@ const revertChanges = async () => {
     const spinner = ora('Reverting changes...');
     spinner.start();
     await $`git reset --hard HEAD~1`;
-    await $`git push origin main --force`;
+    await $`git pull origin main --force`;
     spinner.succeed();
 }
 
