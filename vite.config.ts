fc162a0e144a04b95d7fc6dc789990a3164f06aa
Vahor <nathan.d0601@gmail.com>
2023-11-14 18:48:24 +0100

    add json report

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        env: {
-            "FORCE_COLOR": "1",
-        },
         coverage: {
             include: ['src/**/*.{ts,tsx}'],
             exclude: ['**/index.ts'],
             all: true,
             provider: 'v8',
+            reporter: ['text', 'json-summary', 'json'],
         },
     },
 })
