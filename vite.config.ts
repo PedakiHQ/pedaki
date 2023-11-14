6e2658553a3aebfab4e2cd8eafd04689e8a9f621
Vahor <nathan.d0601@gmail.com>
2023-11-14 18:58:20 +0100

    disable watch by default

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        watch: false,
         coverage: {
             include: ['src/**/*.{ts,tsx}'],
             exclude: ['**/index.ts'],
             all: true,
             provider: 'v8',
-            reporter: ['text', 'json-summary', 'json'],
+            reporter: ['json-summary', 'json'],
+
+            lines: 60,
+            branches: 60,
+            functions: 60,
+            statements: 60
         },
     },
 })
