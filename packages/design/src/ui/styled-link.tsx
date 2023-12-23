7b5540ee59f67738ba670d5ed8391028f2719145
Vahor <me@vahor.fr>
2023-12-23 17:06:43 +0100

    feat: add white variant for StyledLink

--- packages/design/src/ui/styled-link.tsx
+++ packages/design/src/ui/styled-link.tsx
@@ -5,10 +5,11 @@ import Link from 'next/link.js';
 import * as React from 'react';
 
 const styledLinkVariants = cva(
-  'inline-flex items-center transition-colors disabled:pointer-events-none data-[disabled]:text-disabled data-[disabled]:decoration-stroke-disabled data-[disabled]:pointer-events-none data-[disabled]:cursor-not-allowed focus-ring underline-offset-4 hover:decoration-2 decoration-1',
+  'inline-flex items-center transition-colors disabled:pointer-events-none data-[disabled]:text-disabled data-[disabled]:decoration-stroke-disabled data-[disabled]:pointer-events-none data-[disabled]:cursor-not-allowed focus-ring hover:decoration-2 decoration-1',
   {
     variants: {
       variant: {
+        white: 'text-white decoration-white',
         gray: 'text-sub decoration-text-sub',
         black: 'text-black decoration-black',
         blue: 'text-blue-base decoration-blue-base',
@@ -20,10 +21,17 @@ const styledLinkVariants = cva(
         hover: 'hover:underline',
         none: undefined,
       },
+      offset: {
+        1: 'underline-offset-1',
+        2: 'underline-offset-2',
+        3: 'underline-offset-3',
+        4: 'underline-offset-4',
+      },
     },
     defaultVariants: {
       variant: 'black',
       decoration: 'hover',
+      offset: 4,
     },
   },
 );
@@ -33,13 +41,19 @@ export type StyledLinkProps = React.ComponentProps<typeof Link> &
     disabled?: boolean;
   };
 
-const StyledLink: React.FC<StyledLinkProps> = ({ className, variant, decoration, ...props }) => {
+const StyledLink: React.FC<StyledLinkProps> = ({
+  className,
+  variant,
+  decoration,
+  offset,
+  ...props
+}) => {
   const { children, ...other } = props;
 
   return (
     <Link
       {...other}
-      className={cn(styledLinkVariants({ variant, decoration }), className)}
+      className={cn(styledLinkVariants({ variant, decoration, offset }), className)}
       data-disabled={other.disabled}
     >
       {children}
